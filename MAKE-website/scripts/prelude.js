a5aef2e89dd3a53940114d8d3227d890cb48ba78
Ethan <edv121@outlook.com>
2022-05-04 21:24:46 -0700

    push

---
+++ MAKE-website/scripts/prelude.js
@@ -0,0 +1,20 @@
+/*****
+ * Buttons lists for
+ * various screens
+*****/
+
+const guest_screen_buttons = [
+    {
+        text: "Login",
+        action: "login"
+    },
+    {
+        text: "Register",
+        action: "register"
+    }
+];
+
+const maker_screen_buttons = [
+    {
+        text: "Logout",
+        
