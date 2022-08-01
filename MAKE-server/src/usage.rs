b0e5cff474bbf4c7f2ff885661b721d83cd6a03b
Ethan <edv121@outlook.com>
2022-08-01 16:33:08 -0700

    remove plotter

--- MAKE-server/src/usage.rs
+++ MAKE-server/src/usage.rs
@@ -1,5 +1,4 @@
 use crate::*;
-use plotters::prelude::*;
 
 #[derive(Default, Debug, Serialize, Deserialize, Clone)]
 pub enum College {
@@ -50,43 +49,6 @@ impl ButtonRecordLog {
             .filter(|r| r.timestamp >= start && r.timestamp <= end)
             .cloned()
             .collect()
-    }
-
-    pub fn make_histogram_png(
-        &self,
-        name: String,
-        histogram_data: Vec<u32>,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        // Make graph based on start/end timing, and the interval, which is in seconds.
-        let root = BitMapBackend::new(&name, (1600, 1200)).into_drawing_area();
-
-        root.fill(&WHITE).unwrap();
-
-        let mut chart = ChartBuilder::on(&root)
-            .x_label_area_size(35)
-            .y_label_area_size(40)
-            .margin(5)
-            .caption("Histogram Test", ("sans-serif", 50.0))
-            .build_cartesian_2d((0u32..10u32).into_segmented(), 0u32..10u32)?;
-
-        chart
-            .configure_mesh()
-            .disable_x_mesh()
-            .bold_line_style(&WHITE.mix(0.3))
-            .y_desc("Count")
-            .x_desc("Bucket")
-            .axis_desc_style(("sans-serif", 15))
-            .draw()?;
-
-        chart.draw_series(
-            Histogram::vertical(&chart)
-                .style(RED.mix(0.5).filled())
-                .data(histogram_data.iter().map(|x| (*x, 1))),
-        )?;
-
-        root.present()?;
-
-        Ok(())
     }
 
     pub fn make_csv(&self) -> Result<(), Box<dyn std::error::Error>> {
