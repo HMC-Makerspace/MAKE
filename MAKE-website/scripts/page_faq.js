13a37e02ee8e49423646a30528021bc62e44a020
Ethan Vazquez <edv121@outlook.com>
2023-10-23 23:19:18 -0700

    update FAQ

--- MAKE-website/scripts/page_faq.js
+++ MAKE-website/scripts/page_faq.js
@@ -1,1 +1,31 @@
-renderMD("md/faq.md", "faq");
+function toggleSelf(event, el) {
+    // If the event was triggered by a child element that is a "a" or a "button", return
+    if (event.target !== el) {
+        if (event.target.tagName === "A" || event.target.tagName === "BUTTON") {
+            return;
+        }
+    }
+
+    // Close all other open faq
+    const faq_list = document.getElementsByClassName("faq-question");
+
+    for (const faq of faq_list) {
+        if (faq !== el) {
+            faq.classList.remove("open");
+        }
+    }
+
+    el.classList.toggle("open");
+}
+
+function addOnclickToAll() {
+    const faq_list = document.getElementsByClassName("faq-question");
+
+    for (const faq of faq_list) {
+        faq.addEventListener("click", (event) => {
+            toggleSelf(event, faq);
+        });
+    }
+}
+
+addOnclickToAll();
