a7d6b748419ce28cbeab1a12c5aff1c1bb955cc6
wil <seank1215@icloud.com>
2024-10-11 22:10:45 -0700

    Amend auto update cache workflow

--- .github/workflows/auto_update_cache.py
+++ .github/workflows/auto_update_cache.py
@@ -25,10 +25,12 @@ for file in script_files:
         for updated_file_name in updated_file_names:
             pattern = rf'<script src="{updated_file_name}\?v=([\d\.]+)"><\/script>'
             if m := re.search(pattern, contents):
+                print(f"Matched {updated_file_name} in {file}!")
                 version_string = m.group(1)
                 version, subversion = map(int, version_string.split("."))
                 subversion += 1
                 replacement = rf'<script src="{updated_file_name}?v={version}.{subversion}"></script>'
                 contents = re.sub(pattern, replacement, contents)
-        with open(file, "w") as fw:
-            fw.write(contents)
+                with open(file, "w") as fw:
+                    print(f"Wrote new contents to {file}.")
+                    fw.write(contents)
