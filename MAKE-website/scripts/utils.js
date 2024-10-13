637cd9cf2d57fb13feacfc9beea4fd1cfd02b0cf
Wil <wil@gmx.com>
2024-10-12 17:04:01 -0700

    Update utils.js

--- MAKE-website/scripts/utils.js
+++ MAKE-website/scripts/utils.js
@@ -275,7 +275,6 @@ function timestampToDate(timestamp) {
 function secondsToHoursMinutes(seconds) {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
-
     return `${hours}h ${minutes}m`;
 }
 
