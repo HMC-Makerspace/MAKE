f36631641f3b8c7f8f766ffee889ccaad09e3512
wil <seank1215@icloud.com>
2024-12-22 21:03:14 -0800

    Try to get tailwind working :/

--- website/components/Home.tsx
+++ website/components/Home.tsx
@@ -3,7 +3,7 @@ import { Button } from "@nextui-org/react";
 export default function Home() {
     return (
         <div className="bg-black size-10">
-            
+            blah
         </div>
     )
 };
