b5a0df097181a84a52568607726bf4b1a5a55e6d
wil <seank1215@icloud.com>
2025-02-12 20:57:58 -0800

    Make PopupAlert and fix table to provide a default ColumnSelect and allow empty content

---
+++ website/components/PopupAlert.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Alert } from "@heroui/react";
+import { AnimatePresence, motion } from "framer-motion";
+
+export default function PopupAlert({
+    isOpen,
+    onOpenChange,
+    color = "success",
+    description,
+    icon,
+    className = "absolute bottom-6 right-6 w-full sm:w-1/4",
+    timeout = 3000,
+}: {
+    isOpen: boolean;
+    onOpenChange: (isOpen: boolean) => void;
+    color?:
+        | "default"
+        | "primary"
+        | "success"
+        | "warning"
+        | "danger"
+        | "secondary";
+    description?: string;
+    icon?: React.ReactNode;
+    className?: string;
+    timeout?: number;
+}) {
+    // Hide the popup after 3 seconds
+    React.useEffect(() => {
+        if (isOpen) {
+            setTimeout(() => {
+                onOpenChange(false);
+            }, timeout);
+        }
+    }, [isOpen]);
+
+    return (
+        <AnimatePresence>
+            {isOpen && (
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                >
+                    <Alert
+                        color={color}
+                        description={description}
+                        icon={icon}
+                        className={className}
+                    />
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
+}
