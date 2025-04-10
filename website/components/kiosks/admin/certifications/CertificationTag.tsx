862b24f61e37482ee1df4a45a8594a1dcb143374
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2025-04-09 21:17:54 -0700

    cleaned up and commented cert code

--- website/components/kiosks/admin/certifications/CertificationTag.tsx
+++ website/components/kiosks/admin/certifications/CertificationTag.tsx
@@ -1,7 +1,6 @@
-// copied wholesale from user roles :)
-
-import { Card, Tooltip } from "@heroui/react";
+import { Card } from "@heroui/react";
 import { useQuery } from "@tanstack/react-query";
+
 import { TCertification } from "common/certification";
 import CVisibilityIcon from "./CVisibilityIcon";
 
@@ -41,6 +40,7 @@ function getForegroundColor(hex: string): string {
     }
 }
 
+// A certification tag similar (but with less rounded edges) to user role tags (see UserRole)
 export default function CertificationTag({ cert_uuid, showVisibility=false }: { cert_uuid: string, showVisibility?: boolean }) {
     const { data, isSuccess, isError } = useQuery<TCertification>({
         queryKey: ["certification", cert_uuid],
@@ -54,7 +54,7 @@ export default function CertificationTag({ cert_uuid, showVisibility=false }: { 
 
     return (
         <Card
-            className="p-1.5 flex flex-row gap-1 w-fit px-2.5 rounded-md"
+            className="p-1.5 flex flex-row gap-1 w-fit px-2.5 rounded-sm"
             style={{ backgroundColor: color }}
             isBlurred={!isSuccess}
         >
