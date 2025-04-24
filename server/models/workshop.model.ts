98de2b588e60d700befa878e9b3f2bd58beaafa1
wil <seank1215@icloud.com>
2025-04-24 00:10:49 -0700

    Actually revert that change, MongoDB does not like maps...

--- server/models/workshop.model.ts
+++ server/models/workshop.model.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
-import type { TWorkshop } from "common/workshop";
+import type { TWorkshop, TWorkshopUserRecord } from "common/workshop";
+
+const WorkshopUserRecord = new mongoose.Schema<TWorkshopUserRecord>({
+    user_uuid: { type: String, required: true },
+    timestamp: { type: Number, required: true },
+});
 
 /**
  * See {@link TWorkshop} documentation for type information.
@@ -16,17 +21,9 @@ export const Workshop = new mongoose.Schema<TWorkshop>(
         timestamp_end: { type: Number, required: true },
         timestamp_public: { type: Number, required: true },
         required_certifications: { type: [String], required: false },
-        rsvp_list: {
-            type: Map, // All Maps have strings as keys
-            of: Number, // Numbers as values
-            required: true,
-        },
+        rsvp_list: { type: [WorkshopUserRecord], required: true },
         users_notified: { type: [String], required: true },
-        sign_in_list: {
-            type: Map, // All Maps have strings as keys
-            of: Number, // Numbers as values
-            required: true,
-        },
+        sign_in_list: { type: [WorkshopUserRecord], required: true },
         images: { type: [String], required: false },
         authorized_roles: { type: [String], required: false },
     },
