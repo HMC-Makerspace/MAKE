17d6ce2ec88ef4e137d2b7a485b199f34c95d02f
Wil Kirkland <wil@gmx.com>
2025-08-01 05:17:40 +0000

    Fix improper constants in email oath client setup

--- server/core/environment.d.ts
+++ server/core/environment.d.ts
@@ -1,9 +1,12 @@
 declare module "bun" {
     interface Env {
         MONGO_URI: string;
+        SITE_URL: string;
         SESSION_SECRET: string;
         IDP_ENTRY_POINT: string;
         IDP_CALLBACK: string;
+        IDP_ISSUER: string;
+        IDP_ID_FORMAT: string;
         ALLOW_INSECURE_LOGIN: boolean;
 
         NODE_ENV: "development" | "production";
