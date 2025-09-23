adddc3591d3d1067702a54f2ade3828c86b5a39f
abe-3 <arock@hmc.edu>
2025-09-22 19:54:08 -0700

    can extend checkouts now

--- website/pages/admin/CheckoutsKiosk.tsx
+++ website/pages/admin/CheckoutsKiosk.tsx
@@ -360,6 +360,7 @@ export default function CheckoutsKiosk() {
                                 checkouts={checkouts}
                                 inventory={inventory ?? []}
                                 users={users}
+                                selectedUser={user}
                                 config={config}
                                 selectedKeys={new Set()}
                                 isLoading={inventoryLoading}
