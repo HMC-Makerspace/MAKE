15c8b861e0b55e5d6e116350539c2c59c42c7dff
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2025-11-09 13:55:03 -0800

    initial workshop waitlist move email

--- server/controllers/workshop.controller.ts
+++ server/controllers/workshop.controller.ts
@@ -10,6 +10,7 @@ import WorkshopReminderTemplate from "email_templates/workshop_reminder";
 import { Logger } from "pino";
 import WorkshopConfirmationTemplate from "email_templates/workshop_confirmation";
 import { getConfig } from "./config.controller";
+import WorkshopWaitlistTemplate from "email_templates/workshop_waitlist_move";
 
 /**
  * Get all workshops in the database
@@ -221,6 +222,7 @@ export async function rsvpToWorkshop(
 export async function cancelRSVPToWorkshop(
     workshop_uuid: UUID,
     user_uuid: UserUUID,
+    logger: Logger,
 ): Promise<TWorkshop | null> {
     const workshop = await getWorkshop(workshop_uuid);
     // If the workshop doesn't exist, the cancellation fails
@@ -237,6 +239,28 @@ export async function cancelRSVPToWorkshop(
     }
     // Remove the user from the rsvp list
     workshop.rsvp_list = workshop.rsvp_list.filter((rsvp) => rsvp.user_uuid != user_uuid);
+
+    // Send email to user moved off the waitlist
+    const firstUserOnWaitlist =
+        workshop.capacity &&
+        workshop.capacity > 0 &&
+        workshop.rsvp_list.length >= workshop.capacity ?
+        workshop.rsvp_list[workshop.capacity-1].user_uuid : null;
+    
+    if (firstUserOnWaitlist) {
+        const user = await getUser(firstUserOnWaitlist);
+        const config = await getConfig();
+
+        if (user && config) {
+            await sendTemplatedEmail(
+                user.email,
+                "Workshop Waitlist Update: " + workshop.title,
+                WorkshopWaitlistTemplate(workshop, user, config),
+                logger,
+            );
+        }
+    }
+
     // Update the workshop in the database
     return workshop.save();
 }
@@ -313,7 +337,7 @@ export async function workshopReminderEmailCron(logger: Logger) {
                 new_sent_times.push(reminder);
             }
         }
-        
+
         if (new_sent_times.length != workshop.reminder_emails_sent.length) {
             await Workshops.updateOne(
                 {
---
+++ server/email_templates/workshop_waitlist_move.tsx
@@ -0,0 +1,54 @@
+import { timestampToZonedDateTime } from "../../website/utils";
+import { TWorkshop } from "common/workshop";
+import { TUser } from "common/user";
+import { TConfig } from "common/config";
+import { getLocalTimeZone, DateFormatter } from "@internationalized/date";
+
+const WorkshopWaitlistTemplate = (
+    workshop: TWorkshop,
+    user: TUser,
+    config: TConfig,
+) => {
+    const zonedStartTime = timestampToZonedDateTime(
+        workshop.timestamp_start,
+        getLocalTimeZone(),
+    );
+    const dateFormatter = new DateFormatter(config.schedule.locale, {
+        month: "long",
+        day: "numeric",
+        weekday: "long",
+        hour: "numeric",
+        minute: "numeric",
+        timeZoneName: "short",
+        timeZone: config.schedule.timezone,
+    });
+    return (
+        <>
+            <h1>Workshop Waitlist Update</h1>
+            <p>
+                Hello {user.name},
+                <br />
+                <br />
+                <b>
+                    You have been moved off the waitlist and onto the RSVP list for
+                    {workshop.title}.
+                </b>
+                <br />
+                <br />
+                As a reminder, the workshop will start on{" "}
+                {dateFormatter.format(zonedStartTime.toDate())}. Please arrive
+                on time, and <b>sign in with the instructor</b>.
+                <br />
+                <br />
+                If you are unable to attend, please cancel on{" "}
+                <a href="https://make.hmc.edu/workshops">make.hmc.edu</a>.
+            </p>
+
+            <footer>
+                <i>This email was sent automatically by MAKE</i>
+            </footer>
+        </>
+    );
+};
+
+export default WorkshopWaitlistTemplate;
