4fe74e60ef81d0f72b1dffaba9821f48bd6b9a1f
Wil <wil@gmx.com>
2026-02-16 19:33:07 -0800

    Quick fix to use an IP sanitizing function ipKeyGenerator

--- server/rate-limiter.ts
+++ server/rate-limiter.ts
@@ -1,39 +1,46 @@
-import { rateLimit, RateLimitRequestHandler } from 'express-rate-limit';
-import MongoDBStore from '@iroomit/rate-limit-mongodb';
+import {
+    rateLimit,
+    RateLimitRequestHandler,
+    ipKeyGenerator,
+} from "express-rate-limit";
+import MongoDBStore from "@iroomit/rate-limit-mongodb";
 
-export function createLimiter(windowMs: number, max: number): RateLimitRequestHandler {
+export function createLimiter(
+    windowMs: number,
+    max: number,
+): RateLimitRequestHandler {
     return rateLimit({
         windowMs: windowMs,
         max: max,
-        message: { 
-            error: 'API rate limit exceeded', 
-            message: 'Too many requests, please slow down', 
-            status: 429
-        }, 
+        message: {
+            error: "API rate limit exceeded",
+            message: "Too many requests, please slow down",
+            status: 429,
+        },
         standardHeaders: true,
         legacyHeaders: false,
         store: new MongoDBStore({
             uri: process.env.MONGO_URI,
-            collectionName: "rate_limits"
-        })
+            collectionName: "rate_limits",
+        }),
     });
-};
+}
 
 export const globalLimiter = rateLimit({
     windowMs: 60 * 1 * 1000, // 1 minute limit
     max: 600, // 600 in one minute
-    message: { 
-            error: 'API rate limit exceeded', 
-            message: 'Too many requests, please slow down', 
-            status: 429
-        }, 
+    message: {
+        error: "API rate limit exceeded",
+        message: "Too many requests, please slow down",
+        status: 429,
+    },
     standardHeaders: true,
     legacyHeaders: false,
     store: new MongoDBStore({
         uri: process.env.MONGO_URI,
-        collectionName: "rate_limits"
+        collectionName: "rate_limits",
     }),
     keyGenerator: (req, res) => {
-        return req.ip + ":" + req.path
-    }
+        return ipKeyGenerator(req.ip || "") + ":" + req.path;
+    },
 });
