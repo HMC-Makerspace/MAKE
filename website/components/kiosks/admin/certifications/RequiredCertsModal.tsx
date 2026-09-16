d726df7971d1b6fdb53b711eed078b783c6b7b77
Wil <wil@gmx.com>
2026-09-16 11:15:33 -0700

    Certs update

--- website/components/kiosks/admin/certifications/RequiredCertsModal.tsx
+++ website/components/kiosks/admin/certifications/RequiredCertsModal.tsx
@@ -64,7 +64,7 @@ export default function RequiredCertsModal<
     ...mutationProps
 }: {
     certifications: TCertification[];
-    parentKitCerts: TRequiredCertificate[] | undefined;
+    parentKitCerts?: TRequiredCertificate[];
     isOpen: boolean;
     onOpenChange: (isOpen: boolean) => void;
 } & (SinglePatch<T> | BatchPatch<T>)) {
